52297a3fa7ce77e870f17aae1770823fa9228b47
Toxies <estebanbplaza@gmail.com>
2024-03-08 13:29:51 +0100

    successfully added a working profile finder

--- profil.py
+++ profil.py
@@ -1,30 +1,39 @@
 import os
 class Profil:
-    def __init__(self, pseudo, progression, ressources):
+    def __init__(self, pseudo="", progression=0.0, ressources=0):
         self.pseudo = pseudo
         self.progression = progression
         self.ressources = ressources
     def ask_pseudo(self):
-        self.pseudo = input("Enter a pseudo : ")
+        while self.pseudo == "":
+            self.pseudo = input("Enter a pseudo : ")
     def save_profil(self):
-        with open('save_profile.txt', "w", encoding='utf-8') as profil_joueur :
-            profil_joueur.write(f"ID : {self.pseudo}\nProgression : {self.progression}\nRessources : {self.ressources}\n")
-            profil_joueur.close()
+        with open('save_profile.txt', "a", encoding='utf-8') as profil_joueur :
+            profil_joueur.write(f"{self.pseudo} {self.progression} {self.ressources}\n")
+
     def get_profil(self):
+        x=0
+        test = False
+        profil1.ask_pseudo()
         with open('save_profile.txt',"r", encoding='utf-8') as read_profil_joueur:
             contenu = read_profil_joueur.read().split()
-            for _ in contenu :
-                pseudo = contenu[2:3]
-                progression = contenu[5:6]
-                ressources = contenu[8:9]
-            var_pseudo = str(pseudo[0])
-            var_progression = float(progression[0])
-            var_ressources = str(ressources[0])
-            read_profil_joueur.close()
-        print(f"Pseudo : {var_pseudo}\nProgression : {var_progression}\nRessouces : {var_ressources}\n")
-        return var_pseudo, var_progression, var_ressources
+            if self.pseudo in contenu :
+                print('Pseudo exits. Gathering Profile info...')
+                while test is False:
+                    if self.pseudo in contenu[x]:
+                        test = True
+                        self.pseudo = contenu[x]
+                        self.progression = contenu[x+1]
+                        self.ressources = contenu[x+2]
+                    else:
+                        x += 3
+            else:
+                print("Pseudo doesnt exist. Creating New Profile")
+                profil1.save_profil()
+                profil1.get_profil()
+        return self.pseudo, self.progression, self.ressources
 
-profil1 = Profil("", 0.25, 204)
-profil1.ask_pseudo()
-profil1.save_profil()
-profil1.get_profil()
+
+profil1 = Profil()
+pseudo, progression, ressources = profil1.get_profil()
+print(f"Pseudo : {pseudo}\nProgression : {progression}\nRessouces : {ressources}\n")
