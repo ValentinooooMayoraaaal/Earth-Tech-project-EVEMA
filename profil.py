ec05f6190a095410936390976fb30a2d4cf09fdc
Toxies <estebanbplaza@gmail.com>
2024-03-12 13:59:20 +0100

    Added other ressources, might change the ressources variables to dict()

--- profil.py
+++ profil.py
@@ -1,15 +1,17 @@
 import os
 class Profil:
-    def __init__(self, pseudo="", progression=0.0, ressources=0):
+    def __init__(self, pseudo="", progression=0.0, ressource_monnaie=0, ressource_dechet=0,ressource_carbone=10):
         self.pseudo = pseudo
         self.progression = progression
-        self.ressources = ressources
+        self.ressource_monnaie = ressource_monnaie
+        self.ressource_dechet = ressource_dechet
+        self.ressource_carbone = ressource_carbone
     def ask_pseudo(self):
         while self.pseudo == "":
             self.pseudo = input("Enter a pseudo : ")
     def save_profil(self):
         with open('save_profile.txt', "a", encoding='utf-8') as profil_joueur :
-            profil_joueur.write(f"{self.pseudo} {self.progression} {self.ressources}\n")
+            profil_joueur.write(f"{self.pseudo} {self.progression} {self.ressource_monnaie} {self.ressource_dechet} {self.ressource_carbone}\n")
 
     def get_profil(self):
         x=0
@@ -24,16 +26,17 @@ class Profil:
                         test = True
                         self.pseudo = contenu[x]
                         self.progression = contenu[x+1]
-                        self.ressources = contenu[x+2]
+                        self.ressource_monnaie = contenu[x+2]
+                        self.ressource_dechet = contenu[x+3]
+                        self.ressource_carbone = contenu[x+4]
                     else:
-                        x += 3
+                        x += 5
             else:
                 print("Pseudo doesnt exist. Creating New Profile")
                 profil1.save_profil()
                 profil1.get_profil()
-        return self.pseudo, self.progression, self.ressources
-
+        return self.pseudo, self.progression, self.ressource_monnaie, self.ressource_dechet, self.ressource_carbone
 
 profil1 = Profil()
-pseudo, progression, ressources = profil1.get_profil()
-print(f"Pseudo : {pseudo}\nProgression : {progression}\nRessources : {ressources}\n")
+pseudo, progression, ressource_monnaie, ressource_dechet, ressource_carbone = profil1.get_profil()
+print(f"Pseudo : {pseudo}\nProgression : {progression}\nRessource Monnaie : {ressource_monnaie}\nRessource Déchet : {ressource_dechet}\nRessource Carbone : {ressource_carbone}\n")
